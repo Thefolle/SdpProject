521cc4955846fc517c77eedfd9ea8071b67fe434
Davide Calarco <s280174@studenti.polito.it>
2021-12-03 09:57:11 +0100

    Improve VehicleSpawningSystem
    Now the spawning is ruled by checking the
    distance of the last-spawned car, rather
    than by exploiting a timer.

--- Version2/Unity/Assets/Components/SpawningPointComponentData.cs
+++ Version2/Unity/Assets/Components/SpawningPointComponentData.cs
@@ -6,12 +6,8 @@ using Unity.Entities;
 public struct SpawningPointComponentData : IComponentData
 {
     public Entity CarPrefab;
-    /// <summary>
-    /// <para>This field tells to the spawning point which is the street it is attached to.</para>
-    /// <para>It is a convenient variable to avoid raycasting and escalating the hierarchy. In future, this data may be
-    /// obtained from the hierarchy, if spawning points become part of streets in the hierarchy.</para>
-    /// </summary>
-    public Entity OwnerStreet;
+
+    public Entity LastSpawnedCar;
 
     public float SpawningPeriod;
     public float CurrentTimeOffset;
--- Version2/Unity/Assets/Systems/VehicleSpawningSystem.cs
+++ Version2/Unity/Assets/Systems/VehicleSpawningSystem.cs
@@ -18,41 +18,47 @@ public class VehicleSpawningSystem : SystemBase
         var getTrackComponentDataFromEntity = GetComponentDataFromEntity<TrackComponentData>();
         var getParentComponentDataFromEntity = GetComponentDataFromEntity<Parent>();
         var getStreetComponentDataFromEntity = GetComponentDataFromEntity<StreetComponentData>();
+        var getLocalToWorldComponentDataFromEntity = GetComponentDataFromEntity<LocalToWorld>();
 
         var deltaTime = World.Time.DeltaTime;
 
         Entities.ForEach((ref SpawningPointComponentData spawningPointComponentData, in LocalToWorld localToWorld, in Rotation rotation) =>
         {
-            /* Manage spawn period*/
-            spawningPointComponentData.CurrentTimeOffset += deltaTime;
-            if (spawningPointComponentData.CurrentTimeOffset < spawningPointComponentData.SpawningPeriod)
+            if (spawningPointComponentData.LastSpawnedCar == Entity.Null || math.distance(getLocalToWorldComponentDataFromEntity[spawningPointComponentData.LastSpawnedCar].Position, localToWorld.Position) > 16)
             {
-                return;
+                /* Manage spawn period*/
+                //spawningPointComponentData.CurrentTimeOffset += deltaTime;
+                //if (spawningPointComponentData.CurrentTimeOffset < spawningPointComponentData.SpawningPeriod)
+                //{
+                //    return;
+                //}
+                //spawningPointComponentData.CurrentTimeOffset -= spawningPointComponentData.SpawningPeriod;
+
+                /* Spawn a new car */
+                entityManager.SetEnabled(spawningPointComponentData.CarPrefab, false);
+                Entity carEntity = entityManager.Instantiate(spawningPointComponentData.CarPrefab);
+                var carHeight = entityManager.GetComponentData<CompositeScale>(carEntity).Value.c1.y;
+
+                entityManager.SetComponentData(carEntity, new Translation { Value = localToWorld.Position + (carHeight * 2) * math.normalize(localToWorld.Up) });
+                entityManager.SetComponentData(carEntity, new Rotation { Value = rotation.Value });
+
+                //var carComponentData = entityManager.GetComponentData<CarComponentData>(carEntity);
+                //entityManager.SetComponentData(carEntity, new CarComponentData { 
+                //    AngularSpeed = carComponentData.AngularSpeed,
+                //    HasJustSpawned = true,
+                //    lastTimeTried = carComponentData.lastTimeTried,
+                //    maxSpeed = carComponentData.maxSpeed,
+                //    rightOvertakeAllowed = carComponentData.rightOvertakeAllowed,
+                //    Speed = carComponentData.Speed,
+                //    TrackId = carComponentData.TrackId,
+                //    tryOvertake = carComponentData.tryOvertake,
+                //    vehicleIsOn = VehicleIsOn.SpawningPoint
+                //});
+
+                spawningPointComponentData.LastSpawnedCar = carEntity;
+
+                entityManager.SetEnabled(carEntity, true);
             }
-            spawningPointComponentData.CurrentTimeOffset -= spawningPointComponentData.SpawningPeriod;
-
-            /* Spawn a new car */
-            entityManager.SetEnabled(spawningPointComponentData.CarPrefab, false);
-            Entity carEntity = entityManager.Instantiate(spawningPointComponentData.CarPrefab);
-            var carHeight = entityManager.GetComponentData<CompositeScale>(carEntity).Value.c1.y;
-
-            entityManager.SetComponentData(carEntity, new Translation { Value = localToWorld.Position + (carHeight * 2) * math.normalize(localToWorld.Up) });
-            entityManager.SetComponentData(carEntity, new Rotation { Value = rotation.Value });
-
-            //var carComponentData = entityManager.GetComponentData<CarComponentData>(carEntity);
-            //entityManager.SetComponentData(carEntity, new CarComponentData { 
-            //    AngularSpeed = carComponentData.AngularSpeed,
-            //    HasJustSpawned = true,
-            //    lastTimeTried = carComponentData.lastTimeTried,
-            //    maxSpeed = carComponentData.maxSpeed,
-            //    rightOvertakeAllowed = carComponentData.rightOvertakeAllowed,
-            //    Speed = carComponentData.Speed,
-            //    TrackId = carComponentData.TrackId,
-            //    tryOvertake = carComponentData.tryOvertake,
-            //    vehicleIsOn = VehicleIsOn.SpawningPoint
-            //});
-
-            entityManager.SetEnabled(carEntity, true);
         }).WithStructuralChanges().Run();
     
     }
