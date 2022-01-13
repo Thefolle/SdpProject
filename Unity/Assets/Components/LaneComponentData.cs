fe415c6911651a5c7867333097a518ce6c722904
MicheleCarb <s277933@studenti.polito.it>
2022-01-13 11:48:01 +0100

    Create version3 branch based on version 3 of project

--- Unity/Assets/Components/LaneComponentData.cs
+++ Unity/Assets/Components/LaneComponentData.cs
@@ -1,7 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
 using Unity.Entities;
 
 [GenerateAuthoringComponent]
 public struct LaneComponentData : IComponentData
 {
-    public int localToStreetId;
+    /// <summary>
+    /// <para>Component data should never be empty, although Unity optimizes the code for void components data. The community
+    /// indeed signals some bugs that occur in that case.</para>
+    /// </summary>
+    private char Pad;
 }
