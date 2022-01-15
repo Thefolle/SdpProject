d61632770a87e7306a54db6e8c8c3309cb7153b9
Davide Calarco <s280174@studenti.polito.it>
2022-01-15 14:23:13 +0100

    Fix DespawningSystem
    Now the system deletes the whole hierarchy of an
    entity.

--- Unity/Assets/Systems/DespawningSystem.cs
+++ Unity/Assets/Systems/DespawningSystem.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Transforms;
+using static UnityEngine.Debug;
 
 /// <summary>
 /// <para>This system acts as a garbage collector for vehicles that reached their destination by destroying them.</para>
+/// <para>In more detail, this system destroys the whole entity hierarchy given a root entity. The whole operation is performed in one frame, so as to keep the simulator in a consistent state.</para>
 /// </summary>
 public class DespawningSystem : SystemBase
 {
@@ -13,10 +17,33 @@ public class DespawningSystem : SystemBase
 
         var entityManager = World.EntityManager;
 
-        Entities.ForEach((in Entity entity, in AskToDespawnComponentData askToDespawnComponentData) =>
+        Entities.ForEach((in AskToDespawnComponentData askToDespawnComponentData, in Entity entity) =>
         {
             if(askToDespawnComponentData.Asked)
-                entityManager.DestroyEntity(entity);
+            {
+                var ecb = new EntityCommandBuffer(Allocator.TempJob); 
+
+                DestroyHierarchy(ecb, entityManager, entity);
+                
+
+                ecb.Playback(entityManager);
+                ecb.Dispose();
+
+            }
+                
         }).WithStructuralChanges().Run();
+    }
+
+    static void DestroyHierarchy(EntityCommandBuffer ecb, EntityManager entityManager, Entity entity)
+    {
+        if (entityManager.HasComponent<Child>(entity))
+        {
+            var children = entityManager.GetBuffer<Child>(entity);
+            foreach (var child in children)
+            {
+                DestroyHierarchy(ecb, entityManager, child.Value);
+            }
+        }
+        ecb.DestroyEntity(entity);
     }
 }
